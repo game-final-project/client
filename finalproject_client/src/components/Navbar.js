9fc4f126a8ba45b291a653dd85b2b388c877fe0a
Reyhan Huditama <reyhan21098@gmail.com>
2019-05-17 14:59:56 +0700

    touch up features

--- finalproject_client/src/components/Navbar.js
+++ finalproject_client/src/components/Navbar.js
@@ -4,22 +4,24 @@ import { NavLink, withRouter } from 'react-router-dom'
 class Navbar extends Component {
     render() {
         return (
-            <nav style={{ backgroundColor: 'gold' }}>
-                <div className="nav-wrapper">
-                    <NavLink to="/" style={{ color: 'black' }} className="brand-logo center">Bomb's Away <span role="img" aria-label="bomb">💣</span></NavLink>
-                    <ul id="nav-mobile" className="left hide-on-med-and-down">
-                        <li>
-                            <a href="_blank" className="modal-trigger" data-target="registerModal" style={{ color: 'black' }}>Register</a>
-                        </li>
-                        <li>
-                            <a href="_blank" className="modal-trigger" data-target="loginModal" style={{ color: 'black' }}>Login</a>
-                        </li>
-                        <li>
-                            <NavLink to="/WebcamTest" style={{ color: 'black' }}>Webcam (TEST)</NavLink>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
+            <div className="navbar-fixed">
+                <nav style={{ backgroundColor: 'gold' }}>
+                    <div className="nav-wrapper">
+                        <NavLink to="/" style={{ color: 'black' }} className="brand-logo center">Bomb's Away <span role="img" aria-label="bomb">💣</span></NavLink>
+                        <ul id="nav-mobile" className="left hide-on-med-and-down">
+                            <li>
+                                <a href="_blank" className="modal-trigger" data-target="registerModal" style={{ color: 'black' }}>Register</a>
+                            </li>
+                            <li>
+                                <a href="_blank" className="modal-trigger" data-target="loginModal" style={{ color: 'black' }}>Login</a>
+                            </li>
+                            <li>
+                                <NavLink to="/WebcamTest" style={{ color: 'black' }}>Webcam (TEST)</NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
         )
     }
 }
--- finalproject_client/src/containers/WebcamTest.js
+++ finalproject_client/src/containers/WebcamTest.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import * as tf from '@tensorflow/tfjs';
 import * as mobilenet from '@tensorflow-models/mobilenet';
 import * as knnClassifier from '@tensorflow-models/knn-classifier';
-import P5Wrapper from 'react-p5-wrapper' 
+import P5Wrapper from 'react-p5-wrapper'
 import sketch from './gameboard/js/main'
 
 
@@ -86,19 +86,20 @@ export default class WebcamTest extends Component {
         const { direction } = this.state
         return (
             <>
+                <div style={{ marginTop: '15px' }}>
+                    <P5Wrapper direction={direction} sketch={sketch} />
+                </div>
                 <div>
-                    <h4>{this.state.direction}</h4>
-                    <h4>{this.state.accuracy}</h4>
-                    <video autoPlay playsInline muted id="webcam" width="500" height="500"></video>
+                    <h6>{this.state.direction}</h6>
+                    <h6>{this.state.accuracy}</h6>
+                    <video autoPlay playsInline muted id="webcam" width="200" height="200"></video>
                     <br />
                     <a href="_blank" onClick={(event) => event.preventDefault()} id="class-a" className="waves-effect waves-teal btn">UP</a>
-                    <a href="_blank" onClick={(event) => event.preventDefault()} id="class-b" className="waves-effect waves-teal btn">RIGHT</a>
-                    <a href="_blank" onClick={(event) => event.preventDefault()} id="class-c" className="waves-effect waves-teal btn">DOWN</a>
-                    <a href="_blank" onClick={(event) => event.preventDefault()} id="class-d" className="waves-effect waves-teal btn">LEFT</a>
+                    <a href="_blank" style={{ marginLeft: '5px' }} onClick={(event) => event.preventDefault()} id="class-b" className="waves-effect waves-teal btn">RIGHT</a>
+                    <a href="_blank" style={{ marginLeft: '5px' }} onClick={(event) => event.preventDefault()} id="class-c" className="waves-effect waves-teal btn">DOWN</a>
+                    <a href="_blank" style={{ marginLeft: '5px' }} onClick={(event) => event.preventDefault()} id="class-d" className="waves-effect waves-teal btn">LEFT</a>
                 </div>
-                <div className="test" >
-                <P5Wrapper direction={direction} sketch={sketch} />
-                </div>
+                <br />
             </>
         )
     }
