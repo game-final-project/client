5e30ccf65b00e7b298b2b90e84625d9a6dd61b5c
Reyhan Huditama <reyhan21098@gmail.com>
2019-05-18 21:37:30 +0700

    touch up tutorial sliders

--- finalproject_client/src/components/Tutorial.js
+++ finalproject_client/src/components/Tutorial.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
-import Up from '../assets/tutorial/up.jpg'
-import Right from '../assets/tutorial/right.jpg'
-import Down from '../assets/tutorial/down.jpg'
-import Left from '../assets/tutorial/left.jpg'
+import Up from '../assets/tutorial/up.png'
+import Right from '../assets/tutorial/right.png'
+import Down from '../assets/tutorial/down.png'
+import Left from '../assets/tutorial/left.png'
 
 export default class Tutorial extends Component {
     render() {
@@ -12,32 +12,21 @@ export default class Tutorial extends Component {
                     <ul className="slides">
                         <li>
                             <img alt="tutorial" src={Up} />
-                            <div className="caption left-align">
-                                <h6 style={{ color: 'black' }} >Example for "Shoot" Command</h6>
-                            </div>
                         </li>
                         <li>
                             <img alt="tutorial" src={Right} />
-                            <div className="caption left-align">
-                                <h6 style={{ color: 'black' }} >Example for "Move Right" Command</h6>
-                            </div>
                         </li>
                         <li>
                             <img alt="tutorial" src={Down} />
-                            <div className="caption left-align">
-                                <h6 style={{ color: 'black' }} >Example for "Idle" Command</h6>
-                            </div>
                         </li>
                         <li>
                             <img alt="tutorial" src={Left} />
-                            <div className="caption left-align">
-                                <h6 style={{ color: 'black' }} >Example for "Move Left" Command</h6>
-                            </div>
                         </li>
                     </ul>
                 </div>
-                <div className="modal-footer" style={{ backgroundColor: '#282c34' }}>
-                    <a href="_blank" style={{ color: 'white' }} onClick={(event) => event.preventDefault()} className="modal-close waves-effect waves-green btn-flat">Skip</a>
+                <div className="modal-footer" style={{ backgroundColor: '#282c34', display: 'flex', justifyContent: 'space-between' }}>
+                    <h6 style={{ color: 'gold' }}>Please train each direction until accuracy is equal to 1!</h6>
+                    <a href="_blank" style={{ color: 'white' }} onClick={(event) => event.preventDefault()} className="modal-close waves-effect waves-green btn">Got it!</a>
                 </div>
             </div>
         )
