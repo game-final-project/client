670591684976eeeea5ebac7eba13a9698fbe41b8
Reyhan Huditama <reyhan21098@gmail.com>
2019-05-18 12:50:55 +0700

    changed font and added carousel template

--- finalproject_client/src/containers/Home.js
+++ finalproject_client/src/containers/Home.js
@@ -5,15 +5,39 @@ import M from 'materialize-css'
 
 export default class Home extends Component {
     componentDidMount() {
-        var elems = document.querySelectorAll('.modal');
-        M.Modal.init(elems)
+        var modal = document.querySelectorAll('.modal');
+        M.Modal.init(modal)
+
+        var carousel = document.querySelectorAll('.carousel');
+        M.Carousel.init(carousel, {
+            fullWidth: true,
+            indicators: true
+        });
     }
 
     render() {
         return (
             <>
-                <div>
-                    <img alt="bomb gif" style={{ height: '200%', width: '200%' }} src="https://i.gifer.com/origin/d7/d7ac4f38b77abe73165d85edf2cbdb9e_w200.gif" />
+                <div class="carousel carousel-slider center">
+                    <div class="carousel-fixed-item center">
+                        <a class="btn waves-effect white grey-text darken-text-2">button</a>
+                    </div>
+                    <div class="carousel-item red white-text" href="#one!">
+                        <h2>First Panel</h2>
+                        <p class="white-text">This is your first panel</p>
+                    </div>
+                    <div class="carousel-item amber white-text" href="#two!">
+                        <h2>Second Panel</h2>
+                        <p class="white-text">This is your second panel</p>
+                    </div>
+                    <div class="carousel-item green white-text" href="#three!">
+                        <h2>Third Panel</h2>
+                        <p class="white-text">This is your third panel</p>
+                    </div>
+                    <div class="carousel-item blue white-text" href="#four!">
+                        <h2>Fourth Panel</h2>
+                        <p class="white-text">This is your fourth panel</p>
+                    </div>
                 </div>
                 <Register {...this.props} />
                 <Login {...this.props} />
