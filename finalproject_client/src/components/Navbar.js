a69b1f1c4fb526cf607b15de626e6bf7b8465bad
William Suryawan <suryawan.william@gmail.com>
2019-05-19 21:11:26 +0700

    revise the nav

--- finalproject_client/src/components/Navbar.js
+++ finalproject_client/src/components/Navbar.js
@@ -24,7 +24,9 @@ class Navbar extends Component {
                                 )
                             }
                             <li>
-                                <NavLink to="/aboutus" style={{ color: 'black' }}>About</NavLink>
+                                <NavLink to="/aboutus" style={{ color: 'black' }}>About</NavLink>   
+                            </li>
+                            <li>
                                 <NavLink to="/Leaderboard" style={{ color: 'black' }}>Leaderboard</NavLink>
                             </li>
                         </ul>
